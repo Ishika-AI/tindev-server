635c3cf78ac0e5dd1d1c89ed5df941e508758ea3
Nidelson Gimenes <nidelson@gmail.com>
2019-08-21 00:11:11 -0300

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-const a = 5
+import express from 'express'
+import cors from 'cors'
 
-console.log(a)
+import routes from './routes'
+
+const server = express()
+
+const PORT = 3333
+
+server.use(express.json())
+server.use(cors())
+server.use(routes)
+
+server.listen(PORT, (): void => {
+  console.log(`[SERVER] Running at http://localhost:${PORT}`)
+})

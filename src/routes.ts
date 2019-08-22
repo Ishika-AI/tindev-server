67348c33e006594ab6576bd5613b1397836a67ec
Nidelson Gimenes <nidelson@gmail.com>
2019-08-21 22:58:46 -0300

    feat: dev store

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Response, Request } from 'express'
 
+import DevsController from './constrollers/DevsController'
+
 const routes = Router()
 
 routes.get('/', (req: Request, res: Response): Response => {
-  return res.json({ message: 'Hello World!' })
+  return res.json({ message: 'TINDEV BACKEND' })
 })
+
+routes.post('/devs', DevsController)
 
 export default routes
